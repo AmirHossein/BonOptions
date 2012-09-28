557916d00e7e6dc9d19ff12b1ee30e1a6c1effef
Amir Hossein <amir.hossein.hp@gmail.com>
2012-09-28 20:49:32 +0330

    Initial

---
+++ README.md
@@ -0,0 +1,323 @@
+# BonOptions
+**BonOptions** is a helper class to handle *getting/setting* actions.
+
+<br />
+
+## Example & Usage:
+### Start
+```php
+$bonOptions = new BonOptions('My Info');
+```
+
+### Simple Getting/Setting
+```php
+$bonOptions->set('name', 'Amir Hossein');
+echo $bonOptions->get('name'); // Amir Hossein
+```
+### Multiple Getting/Setting
+```php
+$bonOptions->set(
+    array(
+        'age' => 25,
+        'city' => 'Tehran'
+    )
+);
+echo $bonOptions->get('city'); // Tehran
+var_dump($bonOptions->get());
+/*
+array(4) {
+    ["name"]   => string(12) "Amir Hossein"
+    ["age"]    => int(26)
+    ["city"]   => string(6) "Tehran"
+    ["gender"] => string(4) "Male"
+}
+*/
+
+var_dump($bonOptions->get('name', 'city'));
+/*
+array(4) {
+    ["name"]   => string(12) "Amir Hossein"
+    ["city"]   => string(6) "Tehran"
+}
+*/
+```
+
+### Using Validators
+```php
+$bonOptions->setReaderValidator(
+    function($key) {
+        if ($key == 'age') {
+            return false;
+        }
+        return true;
+    }
+);
+var_dump($bonOptions->get('age')); // NULL
+
+$bonOptions->setReaderValidator(function($key){ return true; }); // Reset ReaderValidator
+
+$bonOptions->setWriterValidator(
+    function($key, $value, $min, $max)
+    {
+        if ($key == 'age') {
+            return ($value >= $min && $value <= $max);
+        }
+        return true;
+    }, array(23, 27)
+);
+$bonOptions->set('age', 20);
+echo $bonOptions->get('age'); // 25
+$bonOptions->set('age', 30);
+echo $bonOptions->get('age'); // 25
+$bonOptions->set('age', 26);
+echo $bonOptions->get('age'); // 26
+```
+
+### Hydration
+```php
+$bonOptions->gender = 'Male';
+var_dump($bonOptions->gender); // NULL
+var_dump(isset($bonOptions->gender)); // bool(false)
+
+$bonOptions->hydration(true);
+$bonOptions->gender = 'Male';
+var_dump($bonOptions->gender); // string(4) "Male"
+var_dump(isset($bonOptions->gender)); // bool(true)
+var_dump($bonOptions->name); // string(12) "Amir Hossein"
+var_dump($bonOptions->get('gender')); // string(4) "Male"
+
+$bonOptions->hydration(false);
+var_dump($bonOptions->gender); // NULL
+var_dump($bonOptions->get('gender')); // string(4) "Male"
+```
+
+### Reset
+```php
+$bonOptions->reset(); // $bonOptions is empty now
+```
+
+### Filter
+```php
+$bonOptions->set('date', time());
+echo $bonOptions->filter('date', function($time) {
+    return date('Y-m-d H:i', $time);
+});
+```
+
+### Class Key
+```php
+$profile = new BonOptions('Profile');
+$profile->set('name', 'TheName');
+$bank = new BonOptions('Bank Info');
+$bank->set('account', '47657789');
+$options = array($profile, $bank);
+foreach ($options as $bonOptions) {
+    if ($bonOptions->key() == 'Profile') {
+        echo 'Name: ' . $bonOptions->get('name'); // Name: TheName
+    }
+    if ($bonOptions->key() == 'Bank Info') {
+        echo 'Account No: ' . $bonOptions->get('account'); // Account No: 47657789
+    }
+}
+```
+
+### Using as config class
+```php
+class Config extends BonOptions
+{
+    /**
+     * @static
+     * @access private
+     * @var Config Instance
+     */
+    static private $self;
+
+    /**
+     * Constructor
+     *
+     * @access private
+     */
+    public function __construct()
+    {
+        trigger_error('Contructor is not public. Please use Config::getInstance() instead.', E_USER_ERROR);
+    }
+
+    /**
+     * Get instance
+     *
+     * @static
+     * @return Config
+     */
+    static public function getInstance()
+    {
+        if (!isset(self::$self)) {
+            self::$self = new BonOptions('Config');
+        }
+        return self::$self;
+    }
+}
+
+Config::getInstance()->set('name', 'Amir');
+echo Config::getInstance()->get('name'); // Amir
+```
+
+<br />
+<br />
+
+## Class Methods
+### Constructor
+Create New Instance
+
+	void __construct([string $keyName]);
+#### Arguments:
+- string $keyName (optional) Optional Key for object
+
+#### Returns:
+- void
+
+<br />
+
+### Set
+Setter
+
+	void set(array|string $key(s) [, mixed|null $value]);
+#### Arguments:
+- Two Arguments
+	- string $key key for option
+	- mixed $value Value for option
+- One Argument
+	- array $keys Key/Value array to add to options
+
+#### Returns:
+- void
+
+<br />
+
+### Get
+Getter
+
+	mixed|array get([string $key1, string $key2 [,string $key3 [,…]]]);
+#### Arguments:
+- No Argument
+- One Argument
+	- string $key Option key
+- More Arguments
+	- string $key1 Option key 1
+	- string $key2 Option key 2
+	- ...
+
+#### Returns:
+- No Argument
+	- array All options
+- One Argument
+	- mixed Option value
+- More Arguments
+	- array Values of given keys
+
+<br />
+
+### Filter
+Filter Data
+
+	mixed|array filter(array|string $key, callable $filterFunction [,array $filterArguments]);
+
+#### Arguments:
+- String key or Array Keys
+- Function to call for each value
+- Optional Arguments for filterFunction
+
+#### Returns:
+- Filtered value(s) of given key(s)
+
+
+<br />
+
+### Has
+Check for key
+
+	bool has(string $key);
+
+#### Arguments
+- Key to check
+
+#### Returns
+- bool True if value exists and is accessible
+
+<br />
+
+### SetReaderValidator
+Set validator function to call while getting
+
+	void setReaderValidator(callable $callable [, array $additionalArguments]);
+
+#### Arguments
+- callable $callable Function to call
+- array $additionalArguments Arguments to pass to callable function. First argument always is $key.
+
+#### Returns
+- void
+
+<br />
+
+### SetWriterValidator
+Set validator function to call while setting
+
+	void setWriterValidator(callable $callable [, array $additionalArguments]);
+
+#### Arguments
+- callable $callable Function to call
+- array $additionalArguments Arguments to pass to callable function. First and second arguments always are $key and $value.
+
+#### Returns
+- void
+
+<br />
+
+### Reset
+Reset options storage
+
+	void reset();
+
+#### Arguments
+- void
+
+#### Returns
+- void
+
+<br />
+
+### Hydration
+To access options via instance properties.
+
+	bool hydration([bool $isHydration]);
+
+#### Arguments
+- bool $isHydration Turn on/off hydration
+
+#### Returns
+- bool Hydration
+
+
+<br />
+
+### Key
+Get/Set instance key
+
+	bool key([string $key]);
+
+#### Arguments
+- string $key Instance Key
+
+#### Returns
+- string Current instance Key
+
+
+<br />
+<br />
+
+## Copyright
+**GPL**
+
+## Author
+**Amir Hossein Hodjati Pour** <_me [at] Amir-Hossein.com_>
+
---
+++ bonoptions.class.php
@@ -0,0 +1,319 @@
+<?php
+/**
+ * BonOptions
+ * Helper class to handle get/set options
+ *
+ * @version 1.00
+ * @created on 2012 Sep 27 - 1391 Shahrivar 6
+ * @author Amir Hossein Hodjati Pour <me@Amir-Hossein.com>
+ */
+class BonOptions
+{
+    /**
+     * @access protected
+     * @var array Options
+     */
+    protected $_options = array();
+    /**
+     * @access protected
+     * @var bool Hydration
+     */
+    protected $_hydration = false;
+    /**
+     * @access protected
+     * @var string Options Key
+     */
+    protected $_key;
+    /**
+     * @access protected
+     * @var callable Reader Validator. bool function(string $key[, additional arguments]);
+     */
+    protected $_readerValidator;
+    /**
+     * @access protected
+     * @var array Additional arguments to append to Reader Validator arguments.
+     */
+    protected $_readerValidatorAdditionalArguments = array();
+    /**
+     * @access protected
+     * @var callable Writer Validator. bool function(string $key, mixed $value[, additional arguments]);
+     */
+    protected $_writerValidator;
+    /**
+     * @access protected
+     * @var array Additional arguments to append to Writer Validator arguments.
+     */
+    protected $_writerValidatorAdditionalArguments = array();
+
+    /**
+     * Construct
+     *
+     * @param string|null $key Optional Key for options
+     * @return \BonOptions
+     */
+    public function __construct($key = null)
+    {
+        $this->key($key);
+        $this->setWriterValidator(function($key, $value){ return true; });
+        $this->setReaderValidator(function($key){ return true; });
+    }
+
+    /**
+     * Hydration Getter/Setter
+     *
+     * @param bool|null $isHydration
+     * @return bool
+     */
+    public function hydration($isHydration = null)
+    {
+        if (isset($isHydration)) {
+            $this->_hydration = (bool)$isHydration;
+        }
+        return $this->_hydration;
+    }
+
+    /**
+     * Setter
+     *
+     * @param string|array $keys
+     *      string: Single keys and using $value as value
+     *      array: Associative array
+     * @param mixed|void $value
+     *      mixed: while $key is string
+     *      void: while $key is array
+     * @return void
+     */
+    public function set($keys, $value = null)
+    {
+        if (is_string($keys)) {
+            $keys = array($keys => $value);
+        }
+        if (is_array($keys)) {
+            foreach ($keys as $key => $value) {
+                if ($this->_writerValidator($key, $value) === true) {
+                    $this->_options[$key] = $value;
+                }
+            }
+        }
+    }
+
+    /**
+     * Getter
+     *
+     * @param string|array|void
+     *      string: One or more keys to retrieve
+     *      array: List of keys as array to get
+     *      void
+     * @return mixed Single mixed value while single string key, else associtive array of values
+     * @example
+     *      $bontions->get(); // Get options as array
+     *      $bontions->get('key1'); // Get value assigned to "key1" as mixed
+     *      $bontions->get('key1', 'key2'); // array('key1'=>val1, 'key2'=>val2)
+     *      $bontions->get( array('key1','key2') ); // array('key1'=>val1, 'key2'=>val2)
+     */
+    public function get()
+    {
+        switch (func_num_args()) {
+            case 0:
+                $options = array();
+                foreach ($this->_options as $key => $value) {
+                    if ($this->has($key) === true) {
+                        $options[$key] = $value;
+                    }
+                }
+                return $options;
+                break;
+            case 1:
+                $key = func_get_arg(0);
+                if (is_string($key) || is_numeric($key)) {
+                    if ($this->has($key) === true) {
+                        return $this->_options[$key];
+                    }
+                } elseif (is_array($key)) {
+                    $values = array();
+                    foreach ($key as $k) {
+                        if ($this->has($k) === true) {
+                            $values[$k] = $this->_options[$k];
+                        }
+                    }
+                    return $values;
+                }
+                break;
+            default:
+                $keys = array();
+                foreach (func_get_args() as $arg) {
+                    if (is_string($arg)) {
+                        $keys[] = $arg;
+                    } elseif (is_array($arg)) {
+                        $keys = array_merge($keys, $arg);
+                    }
+                }
+                $values = array();
+                foreach ($keys as $k) {
+                    if ($this->has($k) === true) {
+                        $values[$k] = $this->_options[$k];
+                    }
+                }
+                return $values;
+                break;
+        }
+        return null;
+    }
+
+    /**
+     * Checker
+     *
+     * @param string $key Key to check
+     * @return bool
+     */
+    public function has($key)
+    {
+        return (array_key_exists($key, $this->_options) && $this->_readerValidator($key) === true);
+    }
+
+    /**
+     * Find and filter values
+     *
+     * @param array|string $key String key or Array Keys
+     * @param callable $filterFunction Function to call for each value
+     * @param array $filterArguments Optional Arguments for filterFunction
+     * @return array|mixed|null
+     */
+    public function filter($key, $filterFunction, array $filterArguments = array())
+    {
+        if (is_string($key)) {
+            $values = $this->_filter(array($key), $filterFunction, $filterArguments);
+            return empty($values) ? null : current($values);
+        }
+        if (is_array($key)) {
+            $values = $this->_filter($key, $filterFunction, $filterArguments);
+            return $values;
+        }
+        return null;
+    }
+
+    /**
+     * Find and filter values
+     *
+     * @access protected
+     * @param array $keys Array Keys
+     * @param callable $filterFunction Function to call for each value
+     * @param array $filterArguments Arguments for filterFunction
+     * @return array
+     */
+    protected function _filter(array $keys, $filterFunction, array $filterArguments)
+    {
+        $values = call_user_func_array(array($this, 'get'), array($keys));
+        foreach ($values as $key => $value) {
+            $values[$key] = call_user_func_array($filterFunction, array_merge(array($value), $filterArguments));
+        }
+        return $values;
+    }
+
+    /**
+     * Reset storage
+     *
+     * @return void
+     */
+    public function reset()
+    {
+        $this->_options = array();
+    }
+
+    /**
+     * Setter validator
+     *
+     * @param string $key Option key
+     * @param mixed $value
+     * @return bool
+     */
+    protected function _writerValidator($key, $value)
+    {
+        $arguments = array_merge(array($key, $value), $this->_writerValidatorAdditionalArguments);
+        return call_user_func_array($this->_writerValidator, $arguments);
+    }
+
+    /**
+     * Getter validator
+     *
+     * @access protected
+     * @param string $key Optioin key
+     * @return bool
+     */
+    protected function _readerValidator($key)
+    {
+        $arguments = array_merge(array($key), $this->_readerValidatorAdditionalArguments);
+        return call_user_func_array($this->_readerValidator, $arguments);
+    }
+
+    /**
+     * Set/Get Key
+     *
+     * @param string|void|NULL String to set key, Void/NULL to get Key
+     * @return string Key
+     */
+    public function key($key = null)
+    {
+        if (isset($key)) {
+            $this->_key = $key;
+        }
+        return $this->_key;
+    }
+
+    /**
+     * Set Reader Validator callback
+     *
+     * @param callable $callable Reader Validator callback
+     * @param array $additionalArguments
+     * @return void
+     */
+    public function setReaderValidator($callable, $additionalArguments = array())
+    {
+        $this->_readerValidator = $callable;
+        $this->_readerValidatorAdditionalArguments = $additionalArguments;
+    }
+
+    /**
+     * Set Writer Validator callback
+     *
+     * @param callable $callable Writer Validator callback
+     * @param array $additionalArguments
+     * @return void
+     */
+    public function setWriterValidator($callable, $additionalArguments = array())
+    {
+        $this->_writerValidator = $callable;
+        $this->_writerValidatorAdditionalArguments = $additionalArguments;
+    }
+
+    /**
+     * Overloads
+     */
+    public function __get($var)
+    {
+        if ($this->_hydration === true) {
+            return $this->get($var);
+        }
+        return null;
+    }
+
+    public function __isset($var)
+    {
+        if ($this->_hydration === true) {
+            return $this->has($var);
+        }
+        return false;
+    }
+
+    public function __set($var, $value)
+    {
+        if ($this->_hydration === true) {
+            $this->set($var, $value);
+        }
+    }
+
+    public function __toString()
+    {
+        return get_class($this) . ($this->_key ? ': ' . $this->_key : 'Instance');
+    }
+}
